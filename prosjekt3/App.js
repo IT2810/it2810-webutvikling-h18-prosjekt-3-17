ab7138d80ffb951861ce980367a70ce47003b134
Erlend Sæther <erlend.sae@hotmail.com>
2018-10-15 12:36:00 +0200

    comments and readme

--- prosjekt3/App.js
+++ prosjekt3/App.js
@@ -4,15 +4,17 @@ import { StyleSheet, FlatList, Text, View } from 'react-native';
 import Tasks from './components/Tasks';
 import Streak from './components/Streak';
 import Appointments from './components/Appointments';
+import Goals from './components/Goals';
 
 export default class App extends React.Component {
 
-
+//rendering all the components that makes up the page
   render() {
     return (
       <View style={styles.container}>
 
       <Streak/>
+      <Goals/>
       <Tasks />
       <Appointments style={{flex: 1}} />
 
--- prosjekt3/components/Streak.js
+++ prosjekt3/components/Streak.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import DatePicker from './Date';
 import moment from 'moment';
-
+//imports Date and the momnet library
 
 
 export default class Streak extends React.Component {
+// initializes the date of the streak start and how many days have passed since that day
   constructor(){
     super()
     this.state = {
@@ -15,19 +16,16 @@ export default class Streak extends React.Component {
   }
 
   getDate(e){
-    if (moment(e).isSame(this.state.streakStart, 'day')){
-      this.setState({
-        days: 0
-      })
-    }
-    else{
+  //
     this.setState({
       days: e.diff(this.state.streakStart,'day')
     })
   }
-  }
+
 
   streakReset(el){
+  //this function checks if yesterdays goals was achived and breaks the streak by setting streakstart to today
+  // and setting days to 0. you now need to start over with the goals
     this.setState({
       days:0,
       streakStart: el
@@ -38,7 +36,7 @@ export default class Streak extends React.Component {
     return (
       <View>
 
-        <DatePicker giveDate={this.getDate.bind(this)} streakBreak={this.streakReset.bind(this)} streak={this.state.days}/>
+        <DatePicker giveDate={this.getDate.bind(this)} streak={this.state.days}/>
 
       </View>
     );
